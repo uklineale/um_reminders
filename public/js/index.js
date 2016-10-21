4bb5d812b90728abb9d0ee83ba5df6e6fdd58c1b
Neel Kuila <neelk1123@gmail.com>
2016-10-21 19:22:34 -0400

    Mongo and form

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,20 @@
 angular.module('umr', [])
   .controller('umrController', ['$http', '$scope', function($http, $scope) {
-    
+    $scope.visits = [];
+
+    $scope.add = function(visit) {
+      $http.post('http://localhost:3000/api/visit', visit);
+    }
+    $scope.send = function(message) {
+      $http.post('http://localhost:3000/api/send', message);
+    }
+    $scope.peek = function() {
+      $http.get('http://localhost:3000/api/peek')
+        .success(function(data, status){
+          $scope.visits = data;
+        })
+        .error(function(data, status){
+          alert('Error');
+        });
+    }
   }]);
--- server.js
+++ server.js
@@ -1,10 +1,62 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
+var mongo = require('mongodb');
+
+var Server = mongo.Server;
+var Db = mongo.Db;
+
 var app = express();
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html');
+//Mongo config
+var server = new Server('localhost', 27017, {auto_reconnect:true});
+var db = new Db('umr', server);
+var init = [
+  {
+    fname: "Latoya",
+    lname: "Lewis",
+    phone: "9194002343",
+    date : "2016-10-25T09:00"
+  }
+];
+
+db.open(function(err, db) {
+  if (err) {
+    console.log(err);
+  } else {
+    db.collection('visits', {strict:true}, function(err, collection){
+      if (err){
+        console.log(err);
+      } else{
+        collection.insert( init, {safe:true}, function(err,result){
+          if (err){
+            console.log(err);
+          }
+        });
+      }
+    });
+  }
 });
+
+// Node config
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+
+app.post('/api/visit', function (req, res){
+  var name = req.body.name;
+  var number = req.body.number;
+  var type = req.body.type;
+  var date = req.body.date;
+
+  console.log('Added a visit: ' + name + number + type + date);
+});
+
+app.post('/api/send', function (req, res){
+  var message = req.body.message;
+
+  //Send twilio
+})
 
 app.listen(3000, function () {
     console.log('Server running: port 3000');
