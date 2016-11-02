9efd0e98c26abf3acf6b4d0982b3f08a3576d857
Neel Kuila <neelk1123@gmail.com>
2016-11-01 23:39:51 -0400

    Post csv to database

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var mongo = require('mongodb');
+var csv = require('csv');
 
 var Server = mongo.Server;
 var Db = mongo.Db;
@@ -43,14 +44,36 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
-app.post('/api/visit', function (req, res){
-  var name = req.body.name;
-  var number = req.body.number;
-  var type = req.body.type;
-  var date = req.body.date;
+app.post('/api/visits', function (req, res){
+  var csv_string = req.body.csv_string;
+  console.log(csv_string);
 
-  console.log('Added a visit: ' + name + number + type + date);
+  var fields = csv_string.split(',');
+
+  var visit = [
+    {
+      "lname" : fields[0],
+      "fname" : fields[1],
+      "number": fields[2],
+      "date"  : fields[3]
+    }
+  ]
+
+  db.collection('visits', {strict:true}, function(err, collection){
+    if (err){
+      console.log(err);
+    } else{
+      collection.insert( visit, {safe:true}, function(err, result){
+        if (err){ console.log(err); }
+      });
+    }
+  });
+
+  res.send("Success: "+csv_string);
+  console.log('Added a visit: ' + name + number + date);
 });
+
+
 
 app.post('/api/send', function (req, res){
   var message = req.body.message;
