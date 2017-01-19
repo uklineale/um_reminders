8194489886a1875fbe219d72f00c6d49c5b4a49b
Neel Kuila <neelk1123@gmail.com>
2017-01-19 18:25:53 -0500

    minor url change

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,7 @@ angular.module('umr', [])
   .controller('umrController', ['$http', '$scope', function($http, $scope) {
     $scope.visits = [1,2,3,4,5,6];
 	//Needs changing based on Linode, also there's gotta be a better way
-	var serverUrl = "http://45.56.101.89:3000/api/";
+	var serverUrl = "http://45.56.101.89/api/";
 
     $scope.add = function(visit) {
       $http.post(serverUrl+'upload', visit);
