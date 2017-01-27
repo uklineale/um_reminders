3bba74a1b38bd4d3947d03b217ec84d2fd22d267
Neel Kuila <neelk1123@gmail.com>
2017-01-27 00:54:21 +0000

    absolute pathnames for server

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ var parse = require('csv-parse');
 var twilio = require('twilio');
 
 //Not vendor - but I am my own product
-var config = require('./config');
+var config = require('/root/um_reminders/config');
 var port = process.env.PORT || 3000;
-var upload = require('./routes/upload')
-var uploads = require('./routes/uploads')
+var upload = require('/root/um_reminders/routes/upload')
+var uploads = require('/root/um_reminders/routes/uploads')
 var app = express();
 
 // Node config
-app.use(express.static('public'));
+app.use(express.static('/root/um_reminders/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
